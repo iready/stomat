818923dbb0694ad9b9e7a379025db0a2ac552591
vocrsz <vocrsz@gmail.com>
2016-08-20 21:41:07 +0500

    some fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
+  var COUNTBOX = "#result";
+
   $('.open-popup-link').magnificPopup({
     type:'inline',
-    midClick: true, 
+    midClick: true,
     removalDelay: 500,
     mainClass: 'mfp-fade'
   });
 
   if (window.location.pathname == '/') new WOW().init();
 
-  initCountBox();
+  if ($(COUNTBOX).length > 0) initCountBox();
+
+  if ($('.affix_handler').length > 0) {
+    $('.affix_handler').addClass('affix')
+    $('.affix').affix({
+      offset: {
+        top: 500,
+        bottom: $('body').height() - $('.ask-me').offset().top
+    }}).css("max-width",($('.container').width() / 4) - 32);
+  }
+  $( window ).resize(function() {
+    $('.affix').css("max-width", $('.container').width() / 4);
+  });
 
   today = (new Date()).setDate((new Date()).getDate());
 
@@ -35,22 +49,21 @@ $(document).ready(function() {
 });
 
 function initCountBox() {
-    var COUNTBOX                     = "#result";
     var HALF_SCREEN_OFFSET = 600;
     var showed                            = true;
 
     $(window).on("scroll", function(){
         if(!showed && $(COUNTBOX).length) return false;
- 
-        var scrolledHeight     = $(window).scrollTop();     
+
+        var scrolledHeight     = $(window).scrollTop();
         var countboxOffset    = $(COUNTBOX).offset().top;
- 
+
         if (scrolledHeight + HALF_SCREEN_OFFSET >= countboxOffset) {
           $(".spincrement").spincrement({
               thousandSeparator: "",
               duration: 5000
           });
- 
+
           showed = false;
         }
     });
